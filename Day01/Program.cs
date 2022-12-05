522f96ee06809a144055322a828a529ad692d39f
Pawel <pawel.j.kroczak@gmail.com>
2022-12-05 18:48:29 +0100

    Day 01 solution upload

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string path = Path.Combine(Environment.CurrentDirectory, "input.txt");
+
+var data = File.ReadAllText(path);
+
+int floor = 0;
+
+for (int i = 0; i < data.Length; i++)
+{
+    if (data[i] == '(')
+    {
+        floor++;
+    }
+    else
+    {
+        floor--;
+    }
+
+    if (floor == -1)
+    {
+        Console.WriteLine(i + 1);
+        break;
+    }
+}
