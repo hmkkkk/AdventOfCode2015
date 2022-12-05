390bef4a86a106d129636d480c38ce7ad8f18d88
Pawel <pawel.j.kroczak@gmail.com>
2022-12-05 21:34:17 +0100

    Day 04 solution upload

---
+++ Day04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+
+string input = "yzbqklnj";
+
+for (int i = 0; i < 10000000; i++)
+{
+    using (MD5 md5 = MD5.Create())
+    {
+        byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input + i.ToString());
+        byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+        var hex = Convert.ToHexString(hashBytes);
+        if (hex.Substring(0, 6) == "000000")
+        {
+            Console.WriteLine(hex);
+            break;
+        }
+    }
+
+}
