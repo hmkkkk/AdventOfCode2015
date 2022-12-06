ec80fd340e0b9d42836afddfa581c1b8c4ad8668
Pawel <pawel.j.kroczak@gmail.com>
2022-12-06 18:54:54 +0100

    Day 05 solution

---
+++ Day05/Program.cs
@@ -0,0 +1,36 @@
+﻿string path = Path.Combine(Environment.CurrentDirectory, "input.txt");
+
+var data = File.ReadAllLines(path);
+
+int niceStrings = 0;
+
+foreach (var line in data)
+{
+    bool lineSplitRepeats = false;
+
+    for (int i = 0; i < line.Length - 2; i++)
+    {
+        string twoLetters = line.Substring(i, 2);
+
+        // the idea is if we use replace at least twice
+        // the length will be shorter than line.Length - 2
+        // otherwise we replace it only once, therefore it would be equal
+
+        if (line.Replace(twoLetters, "").Length < line.Length - 2)  
+        {
+            lineSplitRepeats = true;
+            break;
+        }
+    }
+
+    for (int i = 0; i < line.Length - 2; i++)
+    {
+        if (line[i] == line[i + 2] && lineSplitRepeats)
+        {
+            niceStrings++;
+            break;
+        }
+    }
+}
+
+Console.WriteLine(niceStrings);
